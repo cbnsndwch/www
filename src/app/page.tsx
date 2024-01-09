9192fd2039ac509c745f06d3c84d71af4e25b952
Sergio Leon <8313760+cbnsndwch@users.noreply.github.com>
2024-01-09 04:21:34 -0500

    chore(home): temporarily hide newsletter form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -363,7 +363,9 @@ export default async function Home() {
                         ))}
                     </div>
                     <div className="space-y-10 lg:pl-16 xl:pl-24">
-                        <Newsletter />
+                        {/* TODO: uncomment this one we've connected the newsletter form to GHL */}
+                        {/* <Newsletter /> */}
+
                         <Resume />
                     </div>
                 </div>
