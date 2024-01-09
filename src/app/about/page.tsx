4ec14bfdab519019853af7f5e5e4bac1fb977f3f
Sergio Leon <8313760+cbnsndwch@users.noreply.github.com>
2024-01-09 04:25:05 -0500

    chore(about): remove lipsum boilerplate from About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -74,40 +74,6 @@ export default function About() {
                     </h1>
                     <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
                         <p>
-                            Paragraph 1. Lorem Ipsum is simply dummy text of the
-                            printing and typesetting industry. Lorem Ipsum has
-                            been the industry&apos;s standard dummy text ever
-                            since the 1500s, when an unknown printer took a
-                            galley of type and scrambled it to make a type
-                            specimen book. It has survived not only five
-                            centuries, but also the leap into electronic
-                            typesetting, remaining essentially unchanged. It was
-                            popularised in the 1960s with the release of
-                            Letraset sheets containing Lorem Ipsum passages, and
-                            more recently with desktop publishing software like
-                            Aldus PageMaker including versions of Lorem Ipsum.
-                        </p>
-                        <p>
-                            Contrary to popular belief, Lorem Ipsum is not
-                            simply random text. It has roots in a piece of
-                            classical Latin literature from 45 BC, making it
-                            over 2000 years old. Richard McClintock, a Latin
-                            professor at Hampden-Sydney College in Virginia,
-                            looked up one of the more obscure Latin words,
-                            consectetur, from a Lorem Ipsum passage, and going
-                            through the cites of the word in classical
-                            literature, discovered the undoubtable source.
-                        </p>
-                        <p>
-                            Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-                            of &quot;de Finibus Bonorum et Malorum&quot; (The
-                            Extremes of Good and Evil) by Cicero, written in 45
-                            BC. This book is a treatise on the theory of ethics,
-                            very popular during the Renaissance. The first line
-                            of Lorem Ipsum, &quot;Lorem ipsum dolor sit
-                            amet..&quot;, comes from a line in section 1.10.32.
-                        </p>
-                        <p>
                             Today, I’m the CTO and Co-Founder of{' '}
                             <Link
                                 href="https://www.chathq.io"
@@ -131,9 +97,9 @@ export default function About() {
                             </Link>
                             ,{' '}
                             <em>
-                                the One-Stop-Shop branding and
-                                marketing partners for thousands of small
-                                businesses across South Florida
+                                the One-Stop-Shop branding and marketing
+                                partners for thousands of small businesses
+                                across South Florida
                             </em>
                             .
                         </p>
