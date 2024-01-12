9f5960c2a75d38a5918d19ea8c9f85aa7d711a27
Sergio Leon <8313760+cbnsndwch@users.noreply.github.com>
2024-01-12 17:01:43 -0500

    feat(speaking): make podcast appearance cards open in a new tab instead of on the current one

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -63,7 +63,7 @@ Card.Title = function CardTitle<T extends React.ElementType = 'h2'>({
 
     return (
         <Component className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-            {href ? <Card.Link href={href}>{children}</Card.Link> : children}
+            {href ? <Card.Link href={href} target='_blank'>{children}</Card.Link> : children}
         </Component>
     );
 };
