b4e83655eb2ac671ec6216d81b06024d19210871
Sergio Leon <8313760+cbnsndwch@users.noreply.github.com>
2024-01-20 18:55:27 -0500

    ci: disable edge runtime

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -2,8 +2,6 @@ import assert from 'assert';
 import * as cheerio from 'cheerio';
 import { Feed } from 'feed';
 
-// export const runtime = 'edge';
-
 export async function GET(req: Request) {
     let siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,11 @@ import EngagementWidget from '@/components/EngagementWidget';
 
 import '@/styles/tailwind.css';
 
-// export const runtime = 'edge';
-
 export const metadata: Metadata = {
     metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
+    twitter:{
+        card: 'summary_large_image',
+    },
     title: {
         template: '%s - Sergio Leon',
         default:
