f7c718a556b3b42631323a425b8999babbbf9b1a
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2025-04-05 13:49:35 -0400

    feat: temporarily disable GHL integration support calls

--- src/app/call/ghl-integration-discovery/page.tsx
+++ src/app/call/ghl-integration-discovery/page.tsx
@@ -1,9 +1,9 @@
 import { type Metadata } from 'next';
 
 import Container from '@/components/Container';
-import GhlCalendar from '@/components/Calendar';
+// import GhlCalendar from '@/components/Calendar';
 
-const CALENDAR_LINK = process.env.NEXT_PUBLIC_CALENDAR_LINK!;
+// const CALENDAR_LINK = process.env.NEXT_PUBLIC_CALENDAR_LINK!;
 
 const title = 'Book a Call with Serge';
 
@@ -13,44 +13,30 @@ export const metadata: Metadata = {
         "In this call we'll discuss your app or service and what it would take to bring it into the HighLevel Marketplace.",
 };
 
-export default function BookGhlIntegartionDiscoveryCall() {
+export default function BookGhlIntegrationDiscoveryCall() {
     return (
         <Container className="mt-16 sm:mt-32">
             <h1 className="mb-16 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
                 {title}
             </h1>
 
+            <p className="mb-8 text-lg text-zinc-600 dark:text-zinc-400">
+                Thank you for your interest in booking a call! Due to my current
+                workload, I am not taking new calls at this time.
+            </p>
+
+            <p className="mb-8 text-lg text-zinc-600 dark:text-zinc-400">
+                In the meantime, feel free to take a look at my recent Open
+                Source projects on GitHub, and follow me on social media for
+                updates.
+            </p>
+
+            {/* 
             <GhlCalendar
                 id="IISw0HjboChm3kmItS5S_1707771903047"
                 link={CALENDAR_LINK}
-            />
-
-            <iframe
-                id="inline-fuxVsAYeDri7WxSorR76"
-                src="https://link.1nationup.com/widget/form/fuxVsAYeDri7WxSorR76"
-                style={{
-                    width: '100%',
-                    height: '100%',
-                    border: 'none',
-                    borderRadius: '3px',
-                }}
-                data-layout="{'id':'INLINE'}"
-                data-trigger-type="alwaysShow"
-                data-trigger-value=""
-                data-activation-type="alwaysActivated"
-                data-activation-value=""
-                data-deactivation-type="neverDeactivate"
-                data-deactivation-value=""
-                data-form-name="GHL Integrations Appointment Request"
-                data-height="654"
-                data-layout-iframe-id="inline-fuxVsAYeDri7WxSorR76"
-                data-form-id="fuxVsAYeDri7WxSorR76"
-                title="GHL Integrations Appointment Request"
-            ></iframe>
-            <script
-                async
-                src="https://link.1nationup.com/js/form_embed.js"
-            ></script>
+            /> 
+            */}
         </Container>
     );
 }
