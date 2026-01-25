48cf321820e92714efdba76f30f23a4771997d84
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2026-01-25 11:59:08 -0500

    style(components/Header): improve contrast for light mode nav elements

--- src/components/Header/DesktopNavigation.tsx
+++ src/components/Header/DesktopNavigation.tsx
@@ -7,7 +7,7 @@ export default function DesktopNavigation(
 ) {
     return (
         <nav {...props}>
-            <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+            <ul className="flex rounded-full bg-zinc-100 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/10 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
                 <NavItem href="/about">About</NavItem>
                 <NavItem href="/posts">Posts</NavItem>
                 {/* <NavItem href="/projects">Projects</NavItem> */}
