0f96a66f7d1793979f08c652a106b4ba7b4157b7
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2026-01-25 15:31:09 -0500

    feat(app): add projects to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,7 @@
 import { MetadataRoute } from 'next';
 
 import { getAllPosts } from '@/lib/posts/utils';
+import { getAllProjects, getAllProjectUpdates } from '@/lib/projects/utils';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     let siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
@@ -38,6 +39,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             lastModified: new Date(),
             changeFrequency: 'daily',
             priority: 0.8
+        },
+        {
+            url: `${siteUrl}/projects`,
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 0.8
         }
     ];
 
@@ -51,6 +58,42 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             lastModified: new Date(post.date),
             changeFrequency: 'monthly',
             priority: 0.65
+        });
+    }
+
+    const projects = await getAllProjects();
+
+    for (let project of projects) {
+        let url = String(new URL(`/projects/${project.slug}`, siteUrl));
+
+        map.push({
+            url,
+            lastModified: new Date(project.date),
+            changeFrequency: 'weekly',
+            priority: 0.75
+        });
+
+        let updatesUrl = String(new URL(`/projects/${project.slug}/updates`, siteUrl));
+        map.push({
+            url: updatesUrl,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.6
+        });
+    }
+
+    const updates = await getAllProjectUpdates();
+
+    for (let update of updates) {
+        let url = String(
+            new URL(`/projects/${update.projectSlug}/updates/${update.slug}`, siteUrl)
+        );
+
+        map.push({
+            url,
+            lastModified: new Date(update.date),
+            changeFrequency: 'monthly',
+            priority: 0.5
         });
     }
 
