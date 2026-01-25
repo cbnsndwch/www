481a7d3ce2b7cc867a429884562120d56f18f463
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2026-01-25 12:58:29 -0500

    fix: disable engagement widget

--- src/components/EngagementWidget.tsx
+++ src/components/EngagementWidget.tsx
@@ -16,35 +16,37 @@ export type EngagementWidgetProps = {
 
 export default function EngagementWidget({
     widgetId,
-    extras,
+    extras
 }: EngagementWidgetProps) {
     useEffect(() => {
         window.engagementContextExtra = Object.assign(
             window.engagementContextExtra || {},
-            extras,
+            extras
         );
     }, [extras]);
 
-    const widgetData = useMemo(
-        () => ({
-            'crossorigin': 1,
-            'type': 'module',
-            'id': 'engagementWidget',
-            'src': 'https://cdn.chatwidgets.net/widget/livechat/bundle.js',
-            'data-env': '//graph.mycrmsupport.net',
-            'data-container': '#engagement-widget-container',
-            'data-instance': widgetId,
-        }),
-        [widgetId],
-    );
-
-    useEffect(() => {
-        const script = document.createElement('script');
-        Object.entries(widgetData).forEach(([k, v]) =>
-            script.setAttribute(k, `${v}`),
-        );
-        document.body.appendChild(script);
-    }, [widgetData]);
+    // TEMPORARILY DISABLED ENGAGEMENT WIDGET due to CSS issues
+    // 
+    // const widgetData = useMemo(
+    //     () => ({
+    //         'crossorigin': 1,
+    //         'type': 'module',
+    //         'id': 'engagementWidget',
+    //         'src': 'https://cdn.chatwidgets.net/widget/livechat/bundle.js',
+    //         'data-env': '//graph.mycrmsupport.net',
+    //         'data-container': '#engagement-widget-container',
+    //         'data-instance': widgetId,
+    //     }),
+    //     [widgetId],
+    // );
+    //
+    // useEffect(() => {
+    //     const script = document.createElement('script');
+    //     Object.entries(widgetData).forEach(([k, v]) =>
+    //         script.setAttribute(k, `${v}`),
+    //     );
+    //     document.body.appendChild(script);
+    // }, [widgetData]);
 
     return <div id="engagement-widget-container" />;
 }
