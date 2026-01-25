fe267b555ea709ac101d1b20a0fb44ea826ec52a
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2026-01-25 16:23:42 -0500

    refactor(components): increase visible project updates to 5

--- src/components/ProjectLayout/index.tsx
+++ src/components/ProjectLayout/index.tsx
@@ -121,7 +121,7 @@ export default function ProjectLayout({
                                     </Link>
                                 </div>
                                 <ul role="list" className="mt-6 space-y-10">
-                                    {updates.slice(0, 3).map(update => (
+                                    {updates.slice(0, 5).map(update => (
                                         <li key={update.slug}>
                                             <Card>
                                                 <Card.Title
