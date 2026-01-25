fd0d1dc2906f62e1fdd48a159352bafd47112c58
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2026-01-25 13:52:47 -0500

    feat(components): add support for linkable tags

--- src/components/Tags/PostTagList.tsx
+++ src/components/Tags/PostTagList.tsx
@@ -6,17 +6,24 @@ export type PostTagLisProps = {
     tags: string[];
     className?: string;
     color?: 'cyan' | 'amber';
+    isFilterable?: boolean;
 };
 
 export default function PostTagList({
     tags,
     className,
-    color = 'cyan'
+    color = 'cyan',
+    isFilterable = true
 }: PostTagLisProps) {
     return (
         <div className={clsx('flex flex-wrap gap-x-3 gap-y-2', className)}>
             {tags.map(tag => (
-                <PostTag key={tag} color={color} title={tag.toLowerCase()} />
+                <PostTag
+                    key={tag}
+                    color={color}
+                    title={tag.toLowerCase()}
+                    href={isFilterable ? `/posts?tag=${tag}` : undefined}
+                />
             ))}
         </div>
     );
